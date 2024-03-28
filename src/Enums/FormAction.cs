3291422b36877af6b0f986b4f93b0d655d70a56a
Anu6is <curtis.mayers@gmail.com>
2024-03-28 19:57:30 -0400

    MudStaticButton: add option to nest button within a form element

--- src/Enums/FormAction.cs
+++ src/Enums/FormAction.cs
@@ -2,6 +2,16 @@
 
 public enum FormAction
 {
+    /// <summary>
+    ///     Sets the button type to submit
+    /// </summary>
     Submit,
-    Reset
+    /// <summary>
+    ///     Sets the button type to reset
+    /// </summary>
+    Reset,
+    /// <summary>
+    ///     Sets the button type to submit and wraps the button within a from element with method="post"
+    /// </summary>
+    Post
 }
