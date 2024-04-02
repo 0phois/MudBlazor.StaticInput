c74a5729bc04d3dc670fe24e6f57c2136f20450d
Anu6is <curtis.mayers@gmail.com>
2024-04-02 11:23:00 -0400

    refresh page when changing components

---
+++ tests/StaticInput.UnitTests.Viewer/Components/Pages/Index.razor.js
@@ -0,0 +1,21 @@
+﻿let loadParams;
+
+export function onLoad() {
+    loadParams = new URLSearchParams(window.location.search);
+}
+
+export function onUpdate() {
+    const updateParams = new URLSearchParams(window.location.search);
+
+    if (updateParams.size == 0) {
+        return;
+    }
+
+    if (updateParams.toString() !== loadParams.toString()) {
+        window.location.reload();
+    }
+}
+
+export function onDispose() {
+    console.log('Home page dispose');
+}
