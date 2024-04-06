c94282b35bb2b2ed551d6123781ae8a5f2a62118
Anu6is <curtis.mayers@gmail.com>
2024-04-05 23:12:30 -0400

    Playwright integration

---
+++ tests/StaticInput.UnitTests/Fixtures/BrowserFixture.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Playwright;
+
+namespace StaticInput.UnitTests.Fixtures
+{
+    public sealed class BrowserFixture : IAsyncLifetime
+    {
+        public IBrowser? Browser { get; private set; }
+
+        private IPlaywright _playwright = null!;
+
+        public async Task InitializeAsync()
+        {
+            _playwright = await Playwright.CreateAsync();
+
+            Browser = await _playwright.Chromium.LaunchAsync(new() { Headless = true });
+        }
+
+        public async Task DisposeAsync()
+        {
+            await Browser!.DisposeAsync();
+
+            _playwright!.Dispose();
+        }
+    }
+
+    [CollectionDefinition("Browser collection")]
+    public class BrowserCollection : ICollectionFixture<BrowserFixture> { }
+}
---
+++ tests/StaticInput.UnitTests/QueryStringExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace StaticInput.UnitTests
+{
+    public static class QueryStringExtension
+    {
+        public static string ToQueryString(this Type type)
+        {
+            return $"?Test={type.Name}";
+        }
+    }
+}
