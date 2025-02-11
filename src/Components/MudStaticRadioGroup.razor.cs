8e30b8e31fa412d24ee8712129f3aefbda4ac60a
Anu6is <curtis.mayers@gmail.com>
2025-02-11 07:24:31 -0400

    - use Mud component so we don't have to maintain possible future style changed - remove unused methods

--- src/Components/MudStaticRadioGroup.razor.cs
+++ src/Components/MudStaticRadioGroup.razor.cs
@@ -1,14 +1,22 @@
 using Microsoft.AspNetCore.Components;
+using MudBlazor.Utilities;
 
-namespace MudBlazor.StaticInput
+namespace MudBlazor.StaticInput;
+
+public partial class MudStaticRadioGroup<T> : MudRadioGroup<T>
 {
-    public partial class MudStaticRadioGroup<T> : MudRadioGroup<T>
-    {
-        /**********************************************
-         * Hide these inherited properties to prevent *
-         * consumers from modifying them directly.    *
-         **********************************************/
-        protected new T? Value { get; set; }
-        protected new EventCallback<T?> ValueChanged { get; set; }
-    }
+    /**********************************************
+     * Hide these inherited properties to prevent *
+     * consumers from modifying them directly.    *
+     **********************************************/
+    protected new T? Value { get; set; }
+    protected new EventCallback<T?> ValueChanged { get; set; }
+
+    private string GetInputClass() =>
+            new CssBuilder("mud-radio-group")
+                .AddClass(InputClass)
+                .Build();
+
+    internal bool GetDisabledState() => Disabled || ParentIsDisabled;
+    internal bool GetReadOnlyState() => ReadOnly || ParentIsReadOnly;
 }
