07b8a1e761e01fad9ccb5f2eeb4efcc8984e664f
Anu6is <curtis.mayers@gmail.com>
2025-07-15 21:07:04 -0400

    RadioGroup: update selected value

--- src/Components/NavMenu/MudStaticNavDrawerToggle.razor.cs
+++ src/Components/NavMenu/MudStaticNavDrawerToggle.razor.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace MudBlazor.StaticInput;
 
