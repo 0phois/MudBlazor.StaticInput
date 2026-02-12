5b8b06210ac342aca0997527d90ae192f87c8637
Anu6is <curtis.mayers@gmail.com>
2026-02-12 08:08:13 -0400

    update to rc1

--- demo/StaticSample/StaticSample/Program.cs
+++ demo/StaticSample/StaticSample/Program.cs
@@ -67,9 +67,9 @@ else
 
 app.UseHttpsRedirection();
 
-app.UseStaticFiles();
 app.UseAntiforgery();
 
+app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
     .AddInteractiveWebAssemblyRenderMode()
